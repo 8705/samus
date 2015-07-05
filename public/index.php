1e2bf763f43fdf79927e8932f8a6217609fa02bf
EgamiYusuke <egpersent@gmail.com>
2015-07-05 23:23:47 +0900

    analyticsと環境変数追加

--- public/index.php
+++ public/index.php
@@ -15,6 +15,9 @@
 <script src="assets/js/jquery.ba-tinypubsub.js"></script>
 <script src="assets/js/myscript.js?v=2"></script>
 <script src="assets/js/image.js"></script>
+<?php if (getenv('APPLICATION_ENV') === 'production'): ?>
+<script src="assets/js/analytics.js"></script>
+<?php endif; ?>
 </head>
 <body>
 <div class="container">
