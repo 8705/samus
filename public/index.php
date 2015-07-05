402178eb790ea226b662e6350bc23d616300f65e
takobonge <shungo.ishino@gmail.com>
2015-07-06 00:18:07 +0900

    コピーボタンのhref消した

--- public/index.php
+++ public/index.php
@@ -26,7 +26,7 @@
         <div class="col-md-8">
             <h3>入力した文字列をコピーできる</h3>
             <textarea id="copytext" class="form-control"></textarea>
-            <a id="copy-btn1" href="#" class="btn btn-success">Copy</a>
+            <a id="copy-btn1" class="btn btn-success">Copy</a>
             <h3>入力した文字数分だけランダム文字列をコピーできる</h3>
             <select id="random_type" class="form-control">
                 <option value="[a-zA-Z0-9]">半角英数字(a-zA-Z0-9)</option>
@@ -37,7 +37,7 @@
                 <option value="[ぁ-んァ-ヶー一-龠]">全角日本語(ぁ-んァ-ヶー一-龠)</option>
             </select>
             <input id="num" type="text" class="form-control" placeholder="100">
-            <a id="copy-btn2" href="#" class="btn btn-success">Copy</a>
+            <a id="copy-btn2" class="btn btn-success">Copy</a>
         </div>
         <div class="col-md-4"></div>
     </div>
