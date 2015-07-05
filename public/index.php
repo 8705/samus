1cec29cdcc3695806b910156622c5a477885c0c5
EgamiYusuke <egpersent@gmail.com>
2015-07-05 15:04:04 +0900

    ほんとすんませんした

--- public/index.php
+++ public/index.php
@@ -8,18 +8,27 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 <link rel="shortcut icon" href="">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="assets/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+<!-- <script src="assets/js/bootstrap.min.js"></script> -->
 <script src="assets/js/jquery.zclip.min.js"></script>
-<script src="assets/js/script.js"></script>
+<!-- <script src="assets/js/script.js"></script> -->
 </head>
 <body>
 	<div class="row">
         <div class="col-md-8">
         	<textarea>hogehoge</textarea>
-			<a id="copy-button" href="#">Copy</a>
+			<a id="copy-button" href="#" class="btn btn-success">Copy</a>
         </div>
         <div class="col-md-4">.col-md-4</div>
 	</div>
+	<script>
+	$(document).ready(function(){
+	  $("#copy-button").zclip({
+	    path: "assets/js/ZeroClipboard.swf",
+	    copy: function(){
+	      return $(this).prev().val();
+	    }
+	  });
+	});</script>
 </body>
 </html>
