85f93de9503a982107684e27aefa51ccc221c5b7
EgamiYusuke <egpersent@gmail.com>
2015-07-12 20:36:50 +0900

    ダブルクリック防止

--- public/assets/js/samus.js
+++ public/assets/js/samus.js
@@ -1,66 +1,3 @@
-// $(function(){
-//   var samus = $('#samus');
-//   var window_W = window.innerWidth;
-//   var ballDefault = {
-//     r:10,
-//     x:20,
-//     y:10
-//   };
-//   var ball = {
-//     r:10,
-//     x:20,
-//     y:10
-//   };
-
-//   function render(ball,samus)
-//   {
-//     samus.css({
-//       width: ball.r,
-//       height: ball.r,
-//       top:ball.y,
-//       right: ball.x,
-//     });
-//   }
-//   render(ball,samus);
-
-//   function reset()
-//   {
-//     $.extend(ball, ballDefault);
-//     render(ball,samus);
-//   }
-
-//   samus.on('mousedown',function(e){
-//     if( e.button === 0 ){
-//       sizeInterval = setInterval(
-//         function(){
-//           ball.r = ball.r + 2;
-//           render(ball,samus);
-//         },20
-//       );
-//     }
-//   });
-//   $(window).on('mouseup',function(e){
-//     if( e.button === 0 ){
-//       clearInterval(sizeInterval);
-//       launch();
-//     }
-//   });
-//   function launch()
-//   {
-//     moveIneterval = setInterval(
-//       function(){
-//         ball.x = ball.x + 20;
-//         render(ball,samus);
-
-//         // 画面をはみ出すと停止
-//         if ( ball.x > window_W ) {
-//           reset();
-//           clearInterval(moveIneterval);
-//         }
-//       },20);
-//   }
-// });
-
 (function($) {
   $.fn.samus = function(config) {
     var Ball;
@@ -69,7 +6,7 @@
     Ball = (function(){
       Ball.W = window.innerWidth;
       Ball.time = 20;
-      Ball.speed = 10;
+      Ball.speed = 5;
       Ball.defaults = {
         r: 10,
         x: 50,
@@ -82,6 +19,7 @@
       function Ball($elm) {
         config        = config || {};
         this.heaping  = false;
+        this.moving   = false;
         this.defaults = $.extend(Ball.defaults, config);
         this.param    = $.extend({},this.defaults);
         this.ball     = $elm;
@@ -96,7 +34,6 @@
       Ball.prototype = {
 
         render: function(){
-          console.log('render');
           this.param.shadow = this.param.r * 0.2;
           this.ball.css({
             width:            this.param.r,
@@ -110,15 +47,13 @@
         },
 
         reset: function(){
-          console.log('reset');
           this.param = $.extend({},this.defaults);
           this.render();
         },
 
         launch: function(e){
-          console.log('launch');
-          if ( !this.heaping ) return;
-          this.heaping = false;
+          if ( this.moving || !this.heaping) return;
+          this.moving = true;
 
           clearInterval(this.sizeId);
 
@@ -130,6 +65,8 @@
               $('body').addClass('bbb');
               clearInterval(moveID);
               this.reset();
+              this.heaping = false;
+              this.moving = false;
             }
           }, this), Ball.time * 0.5);
         },
